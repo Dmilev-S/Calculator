ff7adcd32c482f95577f8dfc1a3ae34c40ed5bae
Dmilev-S <skvortsov.05@mail.ru>
2021-09-01 16:00:39 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1 = 0; double num2 = 0;
+
+            Console.WriteLine("Консольный калькулятор в C#\r");
+            Console.WriteLine("-------------------------\n");
+
+            Console.WriteLine("Введите номер, а затем нажмите клавишу Ввод");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите другой номер, а затем нажмите клавишу Ввод");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите вариант из следующего списка");
+            Console.WriteLine("\ta - Плюс");
+            Console.WriteLine("\ts - Минус");
+            Console.WriteLine("\tm - Умножить");
+            Console.WriteLine("\td - Поделить");
+            Console.Write("Ваш вариант?");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Ваш результат: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Ваш результат: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Ваш результат: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Ваш результат: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть приложение консоли калькулятора...");
+            Console.ReadKey();            
+        }
+    }
+}
